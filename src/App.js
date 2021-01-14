e6554b20b8e0ce1bce4f75bb7f52184d2f480cee
Pache <serpache@ucm.es>
2021-01-14 23:21:44 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 
 import Header from './components/Header'
 import Formulario from './components/Formulario'
+import ListadoNoticias from './components/ListadoNoticias'
 
 
 function App() {
 
   //states
   const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      //Clave personal de newsAPI, registrarse para obtener una
+      const claveAPI = "";
+      //2-letter ISO 3166-1 code of the country
+      const codigo = "us";
+
+      const url = `http://newsapi.org/v2/top-headlines?country=${codigo}&category=${categoria}&apiKey=${claveAPI}`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria])
 
   return (
     <Fragment>
@@ -16,6 +35,10 @@ function App() {
       <div className="container white">
         <Formulario
           guardarCategoria={guardarCategoria}
+        />
+
+        <ListadoNoticias 
+           noticias={noticias}
         />
       </div>
     </Fragment>
